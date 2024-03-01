4a7b8ade40046f4aac98bc104460a7e9af0c00bd
Connor Williams <conjob.2010@gmail.com>
2024-03-01 14:17:33 +1300

    Added pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,9 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
   },
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent),
+  },
+
 ];
