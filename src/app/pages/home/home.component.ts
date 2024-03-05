0d24912da70320c8418c90483e9be89d4393bdd8
Connor Williams <conjob.2010@gmail.com>
2024-03-05 15:34:02 +1300

    Added styling to layouts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {LayoutComponent} from "../../components/layout/layout.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    RouterLink
-  ],
+    imports: [
+        RouterLink,
+        LayoutComponent
+    ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
