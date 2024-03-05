a1649f7b5a05210b926011eedb2fb8d956467737
Connor Williams <conjob.2010@gmail.com>
2024-03-05 23:25:53 +1300

    Added Kei Hara image

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -14,20 +14,34 @@ import {LayoutComponent} from "../../components/layout/layout.component";
 })
 export class HomeComponent implements AfterViewInit {
   @ViewChild("canvas") canvas?: ElementRef;
+  @ViewChild("canvasFrame") canvasFrame?: ElementRef;
+  @ViewChild("image") image?: ElementRef;
 
   ngAfterViewInit(): void {
-    if (this.canvas === undefined) {
+    if (this.canvas === undefined || this.canvasFrame === undefined) {
       console.log("Canvas not found");
       return;
     }
 
     const ctx = this.canvas.nativeElement.getContext("2d");
-    const canvasWidth = this.canvas.nativeElement.width;
-    const canvasHeight = this.canvas.nativeElement.height;
-    ctx.fillStyle = "green";
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    const canvasFrame = this.canvasFrame.nativeElement;
 
-    console.log("Canvas found")
+    ctx.canvas.width = canvasFrame.clientWidth;
+    ctx.canvas.height = canvasFrame.clientHeight;
+    console.log("Canvas", ctx.canvas.width, ctx.canvas.height)
+
+    const img = new Image();
+    img.onload = () => {
+
+      const x = (ctx.canvas.width - img.width * 0.3) / 2;
+      const y = (ctx.canvas.height - img.height * 0.3) / 2;
+      
+      ctx.drawImage(img, x, y, img.width * 0.3, img.height * 0.3);
+
+
+    }
+    img.src = "https://media.licdn.com/dms/image/D5603AQHNteNbj0wR1w/profile-displayphoto-shrink_800_800/0/1705715328463?e=1715212800&v=beta&t=wVcIyNOl3h_1b_FK7aloU_aWNC7fHl9UrdbQhSdj1XU";
+
   }
 
   // add click listener
