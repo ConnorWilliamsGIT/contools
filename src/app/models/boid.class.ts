5a31ae327cb1956b25444796aabe2b4f804b70b7
Connor Williams <conjob.2010@gmail.com>
2025-03-04 17:56:57 +1300

    Expanded functionality of boids

--- src/app/models/boid.class.ts
+++ src/app/models/boid.class.ts
@@ -1,23 +1,28 @@
 import {coordinate} from './coordinate.type';
 
 export class Boid {
-  public pos : coordinate = {x: 0, y: 0};
-  public vel : coordinate = {x: 0, y: 0};
-  public heading : number = 0;
+  public pos: coordinate = {x: 0, y: 0};
+  public vel: coordinate = {x: 0, y: 0};
+  public heading: number = 0;
 
-  private maxSpeed : number = 300;
-  private minSpeed : number = 80;
-  private protectedRange : number = 50;
-  private visualRange : number = 100;
-  private separationFactor : number = 0.05;
-  private alignmentFactor : number = 0.01;
-  private cohesionFactor : number = 0.0005;
-  private turnFactor : number = 4;
-  private timeLast : number = 0;
+  public maxSpeed: number = 300;
+  public minSpeed: number = 150;
+  public protectedRange: number = 20;
+  public visualRange: number = 80;
+  public separationFactor: number = 0.05;
+  public alignmentFactor: number = 0.04;
+  public cohesionFactor: number = 0.0001;
+  public turnFactor: number = 18;
+  public timeLast: number = 0;
 
-  constructor(x: number, y: number, heading: number = 0) {
+  public xIndex: number = 0;
+  public yIndex: number = 0;
+
+  constructor(x: number, y: number, heading: number = 0.01) {
     this.pos.x = x;
     this.pos.y = y;
+    this.xIndex = Math.floor(this.pos.x / (2 * this.visualRange));
+    this.yIndex = Math.floor(this.pos.y / (2 * this.visualRange));
     this.heading = heading;
     this.timeLast = Date.now();
   }
@@ -27,22 +32,22 @@ export class Boid {
     this.pos.y = coordinate.y;
   }
 
-  computePosition(boids: Boid[], distances: number[][], thisIndex: number, margin: {left: number, right: number, top: number, bottom: number}) {
-    let xVelAvg : number = 0;
-    let yVelAvg : number = 0;
+  computePosition(boids: Boid[], distances: Map<Boid, Map<Boid, number>>, margin: {
+    left: number,
+    right: number,
+    top: number,
+    bottom: number
+  }) {
+    let xVelAvg: number = 0;
+    let yVelAvg: number = 0;
 
-    let xPosAvg : number = 0;
-    let yPosAvg : number = 0;
+    let xPosAvg: number = 0;
+    let yPosAvg: number = 0;
 
-    let neighborCount : number = 0;
+    let neighborCount: number = 0;
 
     boids.forEach((boid, index) => {
-      if (index === thisIndex) {
-        return;
-      }
-
-
-      const distance = distances[thisIndex][index];
+      const distance = distances.get(this)?.get(boid) || 0;
       if (distance < this.protectedRange) {
         this.vel.x += (this.pos.x - boid.pos.x) * this.separationFactor;
         this.vel.y += (this.pos.y - boid.pos.y) * this.separationFactor;
@@ -81,7 +86,12 @@ export class Boid {
       this.vel.y -= this.turnFactor;
     }
 
-    const speed = Math.sqrt(this.vel.x * this.vel.x + this.vel.y * this.vel.y);
+    let speed = Math.sqrt(this.vel.x * this.vel.x + this.vel.y * this.vel.y);
+
+    if (this.vel.x * this.vel.x + this.vel.y * this.vel.y <= 0) {
+      speed = 0;
+    }
+
     if (speed > this.maxSpeed) {
       this.vel.x = (this.vel.x / speed) * this.maxSpeed;
       this.vel.y = (this.vel.y / speed) * this.maxSpeed;
@@ -90,19 +100,22 @@ export class Boid {
       this.vel.x = (this.vel.x / speed) * this.minSpeed;
       this.vel.y = (this.vel.y / speed) * this.minSpeed;
     }
+    if (speed == 0) {
+      this.vel.x = this.minSpeed * Math.sin(this.heading + Math.PI);
+      this.vel.y = -this.minSpeed * Math.cos(this.heading);
+    }
 
     let timeStep = this.getTimeStep();
-
     this.pos.x += this.vel.x * timeStep;
     this.pos.y += this.vel.y * timeStep;
 
-    this.heading = Math.atan2(this.vel.y, this.vel.x) + Math.PI/2;
+    this.heading = Math.atan2(this.vel.y, this.vel.x) + Math.PI / 2;
   }
 
   getTimeStep() {
     const timeNow = Date.now();
     const timeDiff = timeNow - this.timeLast;
     this.timeLast = timeNow;
-    return timeDiff/1000;
+    return timeDiff / 1000;
   }
 }
--- src/app/pages/boids/boids.component.ts
+++ src/app/pages/boids/boids.component.ts
@@ -21,9 +21,17 @@ export class BoidsComponent implements AfterViewInit {
   @ViewChild("resetButton") resetButton?: ElementRef;
 
   private ctx: any;
+  private boidCount: number = 500;
+  private boidHeight: number = 7;
+  private boidScale: number = 0.7;
   private boidList: Boid[] = [];
-
-
+  private gridLists: any[][] = [];
+  private testBoid: Boid = new Boid(0, 0);
+  private timeLoop: any;
+  private paused: boolean = false;
+  private showGrid: boolean = false;
+  private showBoidRange: boolean = false;
+  private margin: number = 100;
 
 
   ngAfterViewInit(): void {
@@ -32,21 +40,22 @@ export class BoidsComponent implements AfterViewInit {
       return;
     }
 
-    // todo add regularly updating spinners
-    // example of code running continuously
-    const p = setInterval(() => {
-      this.computeBoids();
-    }, 10)
-    // setTimeout(() => clearInterval(p), 5000);
-
     this.ctx = this.canvas.nativeElement.getContext("2d");
 
     // this.boidList.push(new Boid(100, 100));
 
     this.resizeCanvas()
 
+    if (!this.paused) {
+      this.paused = true;
+      this.toggleTime();
+    }
+    // this.gridLists = new Array(Math.ceil(this.ctx.canvas.width / (2 * this.testBoid.visualRange))).fill([]).map(() => new Array(Math.ceil(this.ctx.canvas.height / (2 * this.testBoid.visualRange))).fill([])); //new Array(Math.ceil(this.ctx.canvas.height / (2 * this.testBoid.visualRange))).fill([])
+    this.gridLists = Array.from({length: Math.ceil(this.ctx.canvas.width / (2 * this.testBoid.visualRange))}, () =>
+      Array.from({length: Math.ceil(this.ctx.canvas.height / (2 * this.testBoid.visualRange))}, () => [])
+    );
     // spawn 10 boids centered in the canvas with a random position away from the center ( not too far )
-    this.spawnBoids(100);
+    this.spawnBoids(this.boidCount);
     this.updateCanvas();
   }
 
@@ -65,10 +74,38 @@ export class BoidsComponent implements AfterViewInit {
     this.updateCanvas();
   }
 
+  toggleTime() {
+    if (this.paused) {
+      this.timeLoop = setInterval(() => {
+        this.computeBoids();
+      }, 10)
+      this.paused = false;
+    } else {
+      clearInterval(this.timeLoop);
+      this.paused = true;
+    }
+  }
 
   updateCanvas() {
     this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
     this.boidList.forEach(boid => this.drawBoid(boid));
+
+    if (this.showGrid) {
+      this.drawGrid();
+    }
+  }
+
+  drawGrid() {
+    this.gridLists.forEach((row, xIndex) => {
+      row.forEach((cell, yIndex) => {
+        if ((xIndex + yIndex) % 2 === 0) {
+          this.ctx.fillStyle = "rgba(255, 0, 0, 0.1)";
+        } else {
+          this.ctx.fillStyle = "rgba(255, 0, 0, 0)";
+        }
+        this.ctx.fillRect(xIndex * 2 * this.testBoid.visualRange, yIndex * 2 * this.testBoid.visualRange, 2 * this.testBoid.visualRange, 2 * this.testBoid.visualRange);
+      });
+    });
   }
 
   drawBoid(boid: Boid) {
@@ -78,48 +115,96 @@ export class BoidsComponent implements AfterViewInit {
     this.ctx.rotate(boid.heading);
 
     this.ctx.beginPath();
-    this.ctx.moveTo(0, -10);
-    this.ctx.lineTo(7, 10);
-    this.ctx.lineTo(-7, 10);
-    this.ctx.lineTo(0, -10);
+    this.ctx.moveTo(0, -this.boidHeight * this.boidScale);
+    this.ctx.lineTo(Math.round(this.boidHeight * this.boidScale * 0.7), this.boidHeight * this.boidScale);
+    this.ctx.lineTo(-Math.round(this.boidHeight * this.boidScale * 0.7), this.boidHeight * this.boidScale);
+    this.ctx.lineTo(0, -this.boidHeight * this.boidScale);
     this.ctx.fillStyle = "white";
     this.ctx.fill();
     this.ctx.closePath();
 
     this.ctx.restore();
+
+    // draw circle around boid at boid visual range with a hole in the middle for the boid avoid range
+    if (this.showBoidRange) {
+      this.ctx.beginPath();
+      this.ctx.arc(boid.pos.x, boid.pos.y, boid.visualRange, 0, 2 * Math.PI, false);
+      this.ctx.arc(boid.pos.x, boid.pos.y, boid.protectedRange, 2 * Math.PI, 0, true);
+      this.ctx.closePath();
+      this.ctx.fillStyle = "rgba(200, 200, 255, 0.1)";
+      this.ctx.fill();
+    }
   }
 
-  spawnBoids(boidCount: number, boidSpawnRange : number = -1, spawnCenter : coordinate = {x: this.ctx.canvas.width/2, y: this.ctx.canvas.height/2}) {
+  spawnBoids(boidCount: number, boidSpawnRange: number = -1, spawnCenter: coordinate = {
+    x: this.ctx.canvas.width / 2,
+    y: this.ctx.canvas.height / 2
+  }) {
     let boidSpawnRangeX;
     let boidSpawnRangeY;
     if (boidSpawnRange === -1) {
-      boidSpawnRangeX = this.ctx.canvas.width-80;
-      boidSpawnRangeY = this.ctx.canvas.height-80;
+      boidSpawnRangeX = this.ctx.canvas.width - 80;
+      boidSpawnRangeY = this.ctx.canvas.height - 80;
     } else {
       boidSpawnRangeX = boidSpawnRange;
       boidSpawnRangeY = boidSpawnRange;
     }
+
     for (let i = 0; i < boidCount; i++) {
-      const x = spawnCenter.x + Math.random() * boidSpawnRangeX - boidSpawnRangeX/2;
-      const y = spawnCenter.y + Math.random() * boidSpawnRangeY - boidSpawnRangeY/2;
+      const x = spawnCenter.x + Math.random() * boidSpawnRangeX - boidSpawnRangeX / 2;
+      const y = spawnCenter.y + Math.random() * boidSpawnRangeY - boidSpawnRangeY / 2;
       this.boidList.push(new Boid(x, y));
+      this.boidList[this.boidList.length - 1].visualRange *= this.boidScale;
+      this.boidList[this.boidList.length - 1].protectedRange *= this.boidScale;
+      this.boidList[this.boidList.length - 1].maxSpeed *= this.boidScale;
+      this.boidList[this.boidList.length - 1].minSpeed *= this.boidScale;
+      this.gridLists[this.boidList[this.boidList.length - 1].xIndex][this.boidList[this.boidList.length - 1].yIndex].push(this.boidList[this.boidList.length - 1]);
+    }
+  }
+
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    let keyPressed = event.key;
+    if (keyPressed === " ") {
+      this.toggleTime();
+    }
+    if (keyPressed === "x") {
+      this.computeBoids();
     }
   }
 
   computeBoids() {
-    const margin = 80;
-
+    let distances: Map<Boid, Map<Boid, number>> = new Map();
+    let nearbyBoids: Map<Boid, Boid[]> = new Map();
+    this.boidList.forEach((boid, index) => {
+      distances.set(boid, new Map());
+      nearbyBoids.set(boid, []);
+      let xIndex = boid.xIndex;
+      let yIndex = boid.yIndex;
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          if (this.gridLists[xIndex + i] !== undefined && this.gridLists[xIndex + i][yIndex + j] !== undefined) {
+            this.gridLists[xIndex + i][yIndex + j].forEach((boid2: Boid) => {
+              if (boid2 !== boid) {
+                const distance = Math.sqrt((boid.pos.x - boid2.pos.x) ** 2 + (boid.pos.y - boid2.pos.y) ** 2);
+                distances.get(boid)?.set(boid2, distance);
+                if (distance < boid.visualRange) {
+                  nearbyBoids.get(boid)?.push(boid2);
+                }
+              }
+            });
+          }
+        }
+      }
+    });
     this.boidList.forEach(boid => {
-      // get distance from each boid to each other boid store in a 2d array distances[][] where first index is the boid index and second index is the other boid index
-      let distances: number[][] = [];
-      this.boidList.forEach((boid2, index) => {
-        distances.push([]);
-        this.boidList.forEach((boid3, index2) => {
-          distances[index].push(Math.sqrt((boid2.pos.x - boid3.pos.x) ** 2 + (boid2.pos.y - boid3.pos.y) ** 2));
-        });
+      boid.computePosition(nearbyBoids.get(boid) || [], distances, {
+        left: 0 + this.margin,
+        right: this.ctx.canvas.width - this.margin,
+        top: 0 + this.margin,
+        bottom: this.ctx.canvas.height - this.margin
       });
 
-      boid.computePosition(this.boidList, distances, this.boidList.indexOf(boid), {left: 0 + margin, right: this.ctx.canvas.width - margin, top: 0 + margin, bottom: this.ctx.canvas.height - margin});
     });
 
     this.updateCanvas();
